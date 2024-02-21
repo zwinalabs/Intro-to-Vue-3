d6f3959f204750b48820cd4c4cd59fbd839a4dc1
Kais <kais.chrif.jobs@gmail.com>
2024-02-21 18:28:19 +0100

    L3-end

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.google.tn/'
+            // solution
         }
     }
 })
