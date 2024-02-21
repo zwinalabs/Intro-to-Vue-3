4f7dd35e0148b87b68530a88d820d209ae5a6b9c
Kais <kais.chrif.jobs@gmail.com>
2024-02-21 18:19:07 +0100

    Commit L2-end

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm fuzzy pair of socks.' 
+            // Solution
+        }
+    }
+})
