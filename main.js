7e4e65d7971184523ee22e4c895be541b3965147
Kais <kais.chrif.jobs@gmail.com>
2024-02-21 18:22:12 +0100

    main L3-start

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            // Solution
-            description: 'A warm fuzzy pair of socks.' 
-            // Solution
+            product: 'Socks'
         }
     }
 })
